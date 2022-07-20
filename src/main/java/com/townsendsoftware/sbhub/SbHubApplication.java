ed3c64d437b6bb0135d6e804a4bb3e97e4130294
Jeff Jones <alchemys@gmail.com>
2022-07-20 14:43:30 -0600

    Initial working version

---
+++ src/main/java/com/townsendsoftware/sbhub/SbHubApplication.java
@@ -0,0 +1,13 @@
+package com.townsendsoftware.sbhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbHubApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/townsendsoftware/sbhub/fibs/components/FibsCalculator.java
@@ -0,0 +1,98 @@
+package com.townsendsoftware.sbhub.fibs.components;
+
+import static com.townsendsoftware.sbhub.fibs.constants.FibsConstants.MAX_ITERATIONS;
+import static com.townsendsoftware.sbhub.fibs.constants.FibsConstants.CURRENT_PROTOBUF_VERSION;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Component;
+
+import com.townsendsoftware.sbhub.protos.FibonacciParameters;
+import com.townsendsoftware.sbhub.protos.FibonacciResponse;
+import com.townsendsoftware.sbhub.protos.FibonacciSequence;
+import com.townsendsoftware.sbhub.protos.ResponseMessage;
+import com.townsendsoftware.sbhub.protos.ResponseLevel;
+
+import org.apache.commons.collections4.CollectionUtils;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class FibsCalculator {
+
+    public FibonacciResponse generateSequence(long seqSeed0, long seqSeed1, int iterations) {
+        FibonacciParameters parameters = FibonacciParameters.newBuilder()
+            .setVersion(CURRENT_PROTOBUF_VERSION)
+            .setSequenceSeed0(seqSeed0)
+            .setSequenceSeed1(seqSeed1)
+            .setIterations(iterations)
+            .build();
+
+        // Assume success
+        ResponseMessage errorMsg = ResponseMessage.newBuilder()
+            .setVersion(CURRENT_PROTOBUF_VERSION)
+            .setLevel(ResponseLevel.SUCCESS)
+            .setMessage("Success")
+            .build();
+
+        List<Long> generatedSequence = new ArrayList<Long>();
+
+        if (seqSeed0 >= 0 && seqSeed1 > 0 && iterations > 0) {
+            if (iterations <= MAX_ITERATIONS) {
+                if (seqSeed0 >= seqSeed1 && seqSeed1 != 1 && seqSeed0 !=1) {
+                    String msgStr = String.format("seqSeed0 must be < seqSeed1 (unless both are 1). Provided: seqSeed0[%s], seqSeed1[%s], iterations[%s]", seqSeed0, seqSeed1, iterations);
+                    log.error(msgStr);
+                    errorMsg = ResponseMessage.newBuilder()
+                        .setVersion(CURRENT_PROTOBUF_VERSION)
+                        .setLevel(ResponseLevel.ERROR)
+                        .setMessage(msgStr)
+                        .build();
+                } else {
+                    log.info("Generating fibonacci sequence using seeds {}, {} for {} iterations", seqSeed0, seqSeed1, iterations);
+                    generatedSequence.add(seqSeed0);
+                    generatedSequence.add(seqSeed1);
+                    for (int seqCount = 2; seqCount < iterations; seqCount++) {
+                        // This should work since we have already seeded the first two values
+                        long toAdd = (generatedSequence.get(seqCount-1).longValue() + generatedSequence.get(seqCount-2).longValue());
+                        generatedSequence.add(toAdd);
+                    } 
+                }
+            } else {
+                String msgStr = String.format(
+                        "provided iterations '%s' exceeds the max number of iterations ('%s') for this application",
+                        iterations, MAX_ITERATIONS);
+                log.error(msgStr);
+                errorMsg = ResponseMessage.newBuilder()
+                    .setVersion(CURRENT_PROTOBUF_VERSION)
+                    .setLevel(ResponseLevel.ERROR)
+                    .setMessage(msgStr)
+                    .build();
+            }
+        } else {
+            String msgStr = String.format(
+                    "seqSeed0, seqSeed1 and iterations must be positive long values. Provided: seqSeed0[%s], seqSeed1[%s], iterations[%s]",
+                    seqSeed0, seqSeed1, iterations);
+            log.error(msgStr);
+            errorMsg = ResponseMessage.newBuilder()
+                .setVersion(CURRENT_PROTOBUF_VERSION)
+                .setLevel(ResponseLevel.ERROR)
+                .setMessage(msgStr)
+                .build();
+        }
+
+        FibonacciSequence fibSequence = FibonacciSequence.newBuilder()
+            .setVersion(CURRENT_PROTOBUF_VERSION)
+            .addAllSequence(generatedSequence)
+            .setCount((CollectionUtils.isNotEmpty(generatedSequence)) ? generatedSequence.size() : 0)
+            .build();
+
+        return FibonacciResponse.newBuilder()
+            .setVersion(CURRENT_PROTOBUF_VERSION)
+            .setSequence(fibSequence)
+            .setParameters(parameters)
+            .setMessage(errorMsg)
+            .build();
+    }
+}
